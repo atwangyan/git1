31c8e0529be7886ca5efdd4ceb538f2277f75b2f
atwangyan <111678029+atwangyan@users.noreply.github.com>
2022-08-22 14:19:39 +0800

    Update GitTest.java

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("pull");
+      
     }
 }
